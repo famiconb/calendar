89207fbfd96d6c64320fe3634bbd55a9c31cccc4
Shunsuke Uchino <news7vip@gmail.com>
2022-05-11 14:13:16 +0900

    Setup jest for testing

---
+++ renderer/utils/fizzbuzz.ts
@@ -0,0 +1,15 @@
+export const createFizzBuzzArray = (n: number): string[] => {
+  const list: string[] = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      list.push("FizzBuzz");
+    } else if (i % 5 === 0) {
+      list.push("Buzz");
+    } else if (i % 3 === 0) {
+      list.push("Fizz");
+    } else {
+      list.push(i.toString());
+    }
+  }
+  return list;
+};
