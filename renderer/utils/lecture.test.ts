0bb054ea94469883ddc496f175d889f544bc9691
Nagarei <Nagarei@users.noreply.github.com>
2022-05-17 17:30:48 +0900

    console.log

--- renderer/utils/lecture.test.ts
+++ renderer/utils/lecture.test.ts
@@ -1,38 +1,35 @@
 import { Lecture } from "../interfaces";
 import { saveLecture, loadLecture } from "./lecture";
 
-
 export function saveLectureTest() {
-    const testdata: Lecture[] = [
-        {
-        id: 0,
-        name: "こうぎ1",
-        dates: [{dayOfWeek: 1, period:[3,4]}],
-        memo: [{title: "zoom", text:"https://A"}],
-        },
-        {
-        id: 2,
-        name: "こうぎ9",
-        dates: [{dayOfWeek: 4, period:[1,2]}],
-        memo: [{title: "zoom", text:"https://B"}],
-        },
-    ];
+  const testdata: Lecture[] = [
+    {
+      id: 0,
+      name: "こうぎ1",
+      dates: [{ dayOfWeek: 1, period: [3, 4] }],
+      memo: [{ title: "zoom", text: "https://A" }],
+    },
+    {
+      id: 2,
+      name: "こうぎ9",
+      dates: [{ dayOfWeek: 4, period: [1, 2] }],
+      memo: [{ title: "zoom", text: "https://B" }],
+    },
+  ];
 
-    localStorage.clear()
-    saveLecture(testdata)
-    const data: Lecture[] = loadLecture()
-    const datastr: string = JSON.stringify(data)
-    const testdatastr: string = JSON.stringify(testdata)
-    global.ipcRenderer.send("message", datastr);
-    global.ipcRenderer.send("message", testdatastr);
-    global.ipcRenderer.send("message", datastr === testdatastr);
-    return datastr === testdatastr;
+  localStorage.clear();
+  saveLecture(testdata);
+  const data: Lecture[] = loadLecture();
+  const datastr: string = JSON.stringify(data);
+  const testdatastr: string = JSON.stringify(testdata);
+  console.log(datastr);
+  console.log(testdatastr);
+  console.log(datastr === testdatastr);
+  return datastr === testdatastr;
 }
 
 describe("test lecture saveload", () => {
-test("with simple data", () => {
+  test("with simple data", () => {
     //expect(saveLectureTest()) //localstrageが使えない
+  });
 });
-
-});
-  
