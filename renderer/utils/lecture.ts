51dd388595c3564d785e0ed315eb588378fa4b71
Nagarei <Nagarei@users.noreply.github.com>
2022-05-17 17:22:03 +0900

    prettier

--- renderer/utils/lecture.ts
+++ renderer/utils/lecture.ts
@@ -1,14 +1,16 @@
 import { Lecture } from "../interfaces";
 
-const STORAGE_KEY = "CUCalendar"
+const STORAGE_KEY = "CUCalendar";
 
 export function loadLecture(): Lecture[] {
-  const data_str = localStorage.getItem(STORAGE_KEY)
-  if (data_str === null) { return []; }
+  const data_str = localStorage.getItem(STORAGE_KEY);
+  if (data_str === null) {
+    return [];
+  }
   return JSON.parse(data_str) as Lecture[];
 }
 export function saveLecture(lectures: Lecture[]) {
-  const data_str: string = JSON.stringify(lectures)
-  localStorage.setItem(STORAGE_KEY, data_str)
+  const data_str: string = JSON.stringify(lectures);
+  localStorage.setItem(STORAGE_KEY, data_str);
   return;
 }
