fff73906c3398c808b583bd7d25df73e22399a8d
Shigenori A <arimichi.s.aa@m.titech.ac.jp>
2022-05-24 16:19:58 +0900

    フォーマットチェック

--- renderer/pages/lecture-info.tsx
+++ renderer/pages/lecture-info.tsx
@@ -4,9 +4,9 @@ import LectureList from "../components/LectureList";
 import { Lecture, LectureDate, LectureMemo, User } from "../interfaces";
 
 type Props = {
-    lecture: Lecture;
-    lectureList: LectureMemo[];
-  };
+  lecture: Lecture;
+  lectureList: LectureMemo[];
+};
 
 const LectureInfoPage = ({ lecture = sampleLectureInfo }: Props) => (
   <Layout title="講義情報 | Next.js + TypeScript + Electron Example">
@@ -19,8 +19,19 @@ const LectureInfoPage = ({ lecture = sampleLectureInfo }: Props) => (
   </Layout>
 );
 
-export const sampleLectureDates: LectureDate[] = [{dayOfWeek: 1, period: [1,2]},{dayOfWeek: 4, period: [3,5]}];
-export const sampleLectureMemo: LectureMemo[] = [{title: "title1", text: "text1"},{title: "title2", text: "text2"}];
-export const sampleLectureInfo: Lecture = { id: 1, name: "サンプル" , dates :sampleLectureDates , memo :sampleLectureMemo};
+export const sampleLectureDates: LectureDate[] = [
+  { dayOfWeek: 1, period: [1, 2] },
+  { dayOfWeek: 4, period: [3, 5] },
+];
+export const sampleLectureMemo: LectureMemo[] = [
+  { title: "title1", text: "text1 \n link" },
+  { title: "title2", text: "text2 \n link" },
+];
+export const sampleLectureInfo: Lecture = {
+  id: 1,
+  name: "サンプル",
+  dates: sampleLectureDates,
+  memo: sampleLectureMemo,
+};
 
 export default LectureInfoPage;
