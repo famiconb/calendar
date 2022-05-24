77869c89853b99f830f0768b63fa2a170b82d185
shizuka <astaroth.meffi.me617@gmail.com>
2022-05-24 15:50:04 +0900

    big calendarのcustom viewを削除

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -10,13 +10,9 @@ import {
   Calendar as BigCalendar,
   dateFnsLocalizer,
   Views,
-  Navigate,
   DateLocalizer,
 } from "react-big-calendar";
 import PropTypes from "prop-types";
-import * as dates from "date-arithmetic";
-// @ts-ignore
-import * as TimeGrid from "react-big-calendar/lib/TimeGrid";
 
 // BigCalendarの日付を日本時刻に
 const locales = {
@@ -42,73 +38,6 @@ const eventList = [
   },
 ];
 
-function MyWeek({
-  date,
-  localizer,
-  max = localizer.endOf(new Date(), "day"),
-  min = localizer.startOf(new Date(), "day"),
-  scrollToTime = localizer.startOf(new Date(), "day"),
-  ...props
-}) {
-  const currRange = useMemo(
-    () => MyWeek.range(date, { localizer }),
-    [date, localizer]
-  );
-
-  return (
-    <TimeGrid
-      date={date}
-      eventOffset={15}
-      localizer={localizer}
-      max={max}
-      min={min}
-      range={currRange}
-      scrollToTime={scrollToTime}
-      {...props}
-    />
-  );
-}
-
-MyWeek.propTypes = {
-  date: PropTypes.instanceOf(Date).isRequired,
-  localizer: PropTypes.object,
-  max: PropTypes.instanceOf(Date),
-  min: PropTypes.instanceOf(Date),
-  scrollToTime: PropTypes.instanceOf(Date),
-};
-
-MyWeek.range = (date, { localizer }) => {
-  const start = date;
-  const end = dates.add(start, 2, "day");
-
-  let current = start;
-  const range = [];
-
-  while (localizer.lte(current, end, "day")) {
-    range.push(current);
-    current = localizer.add(current, 1, "day");
-  }
-
-  return range;
-};
-
-MyWeek.navigate = (date, action, { localizer }) => {
-  switch (action) {
-    case Navigate.PREVIOUS:
-      return localizer.add(date, -3, "day");
-
-    case Navigate.NEXT:
-      return localizer.add(date, 3, "day");
-
-    default:
-      return date;
-  }
-};
-
-MyWeek.title = (date) => {
-  return `My awesome week: ${date.toLocaleDateString()}`;
-};
-
 const IndexPage = () => {
   // イベントクリック
   const onSelectEvent = useCallback((calEvent) => {
@@ -126,9 +55,7 @@ const IndexPage = () => {
         today: "今週",
       },
       views: {
-        // week:MyWeek,
         week: true,
-        // day:true,
         agenda: true,
       },
     }),
