40160ef34e230d5f40197152eac4bd5ae68aa670
Shigenori A <arimichi.s.aa@m.titech.ac.jp>
2022-05-24 15:56:55 +0900

    useMemoの削除

--- renderer/components/LectureList.tsx
+++ renderer/components/LectureList.tsx
@@ -4,53 +4,56 @@ import { Lecture, LectureDate, LectureMemo, User } from "../interfaces";
 import { stringify } from "querystring";
 
 type Props = {
-  lecture: Lecture
+  lecture: Lecture;
 };
 
-const LectureList = ({lecture}: Props) => (
+const getStringOfDayOfWeek = (item: { dayOfWeek: any }) => {
+  switch (item.dayOfWeek) {
+    case 0:
+      return "日";
+    case 1:
+      return "月";
+    case 2:
+      return "火";
+    case 3:
+      return "水";
+    case 4:
+      return "木";
+    case 5:
+      return "金";
+    case 6:
+      return "土";
+    default:
+      return "曜日設定が間違っているぞ!!";
+  }
+};
+
+const LectureList = ({ lecture }: Props) => (
   <ul>
     <h1>講義情報ページ</h1>
     <h2>講義名: {lecture.name}</h2>
     <h3>講義の日程</h3>
-    {lecture.dates.map((item,i) => (
+    {lecture.dates.map((item, i) => (
       <li key={i}>
-        {
-          useMemo( () => {
-            switch(item.dayOfWeek){
-              case 0:
-                return "日"
-              case 1:
-                return "月"
-              case 2:
-                return "火"
-              case 3:
-                return "水"
-              case 4:
-                return "木"
-              case 5:
-                return "金"
-              case 6:
-                return "土"
-              default:
-                return "曜日設定が間違っているぞ!!"
-          }},[item])
-        }曜日<br></br>
-        時限: {
-          useMemo( () => {let tmp : String = "";
-            if(item.period.length != 2){
-              return "期間のサイズがおかしい!!";
-            }else{
-              return item.period[0] + "~" + item.period[1];
-            }
-          }, [item])
-        }
+        {getStringOfDayOfWeek(item)}
+        曜日<br></br>
+        時限:{" "}
+        {() => {
+          const tmp: String = "";
+          if (item.period.length != 2) {
+            return "期間のサイズがおかしい!!";
+          } else {
+            return item.period[0] + "~" + item.period[1];
+          }
+        }}
       </li>
     ))}
     <h3>講義に関するメモ</h3>
-    {lecture.memo.map((item,i) => (
+    {lecture.memo.map((item, i) => (
       <li key={i}>
-        {item.title}<br></br>
-        <input type = "text" value = {item.text} contentEditable = {false}/>
+        {item.title}
+        <br></br>
+        <textarea value={item.text} contentEditable={false} />
       </li>
     ))}
   </ul>
