f1a8ef10d9355880e9c98a0020ff049c7e67d428
Nagarei <Nagarei@users.noreply.github.com>
2022-05-27 22:04:33 +0900

    クエリパラメータ対応

--- renderer/pages/lecture-info.tsx
+++ renderer/pages/lecture-info.tsx
@@ -1,23 +1,43 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import Layout from "../components/Layout";
 import LectureList from "../components/LectureList";
 import { Lecture, LectureDate, LectureMemo, User } from "../interfaces";
+import { loadLecture } from "../utils/lecture";
 
-type Props = {
-  lecture: Lecture;
-  lectureList: LectureMemo[];
+const LectureInfoErrorPage = () => {
+  return (
+    <Layout title={`Error`}>
+      <p>
+        <span style={{ color: "red" }}>Error:</span> lecture not found
+      </p>
+    </Layout>
+  );
 };
+const LectureInfoPage = () => {
+  const router = useRouter();
+  const query_id_raw = router.query["id"];
+  if (query_id_raw === undefined) {
+    return LectureInfoErrorPage();
+  }
+  try {
+    const id = Array.isArray(query_id_raw) ? query_id_raw[0] : query_id_raw;
+    const lecture = findLecture(id);
 
-const LectureInfoPage = ({ lecture = sampleLectureInfo }: Props) => (
-  <Layout title="講義情報 | Next.js + TypeScript + Electron Example">
-    <LectureList lecture={lecture} />
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
-  </Layout>
-);
+    return (
+      <Layout title="講義情報 | Next.js + TypeScript + Electron Example">
+        <LectureList lecture={lecture} />
+        <p>
+          <Link href="/">
+            <a>Go home</a>
+          </Link>
+        </p>
+      </Layout>
+    );
+  } catch (err: any) {
+    return LectureInfoErrorPage();
+  }
+};
 
 const sampleLectureDates: LectureDate[] = [
   { dayOfWeek: 1, period: [1, 2] },
@@ -33,5 +53,16 @@ const sampleLectureInfo: Lecture = {
   dates: sampleLectureDates,
   memo: sampleLectureMemo,
 };
+
+export function findLecture(id: number | string) {
+  const lectures: Lecture[] = loadLecture();
+  const selected = lectures.find((lec) => lec.id === Number(id));
+
+  if (!selected) {
+    throw new Error("Cannot find user");
+  }
+
+  return selected;
+}
 
 export default LectureInfoPage;
