e27d0a1ebc270b43e9fcec61c6b9439cae0773d7
Shigenori A <arimichi.s.aa@m.titech.ac.jp>
2022-06-14 17:20:19 +0900

    講義情報の表示

--- renderer/pages/lecture-info.tsx
+++ renderer/pages/lecture-info.tsx
@@ -1,16 +1,21 @@
+import React, { useState } from 'react'
+import { BrowserRouter, Router, useLocation } from 'react-router-dom'
+
 import Link from "next/link";
 import Layout from "../components/Layout";
 import LectureList from "../components/LectureList";
 import { Lecture, LectureDate, LectureMemo, User } from "../interfaces";
+import { loadLecture } from '../utils/lecture';
+import { useRouter } from 'next/router';
 
 type Props = {
   lecture: Lecture;
   lectureList: LectureMemo[];
 };
 
-const LectureInfoPage = ({ lecture = sampleLectureInfo }: Props) => (
+const LectureInfoPage = () => (
   <Layout title="講義情報 | Next.js + TypeScript + Electron Example">
-    <LectureList lecture={lecture} />
+    <LectureList lecture={getLectureData()} />
     <p>
       <Link href="/">
         <a>Go home</a>
@@ -18,6 +23,45 @@ const LectureInfoPage = ({ lecture = sampleLectureInfo }: Props) => (
     </p>
   </Layout>
 );
+
+function getLectureData():Lecture {
+  const router = useRouter();
+  const query_id_raw = router.query["id"];
+
+  if (query_id_raw === undefined) {
+    console.log("query is undefined")
+    return sampleLectureInfo;
+  }
+  try {
+    const id = Array.isArray(query_id_raw) ? query_id_raw[0] : query_id_raw;
+    const lecture = findLecture(id);
+    return lecture;
+  }catch(e:any){
+    console.log("error in findLecture")
+  }
+  return sampleLectureInfo;
+}
+
+function findLecture(id: number | string) {
+  const lectures: Lecture[] = loadLecture();
+  const found = lectures.find((lec) => lec.id === Number(id));
+
+  if (!found) {
+    throw new Error("Cannot find lecture");
+  }
+
+  return found;
+}
+
+function getLectureDataFromDataList(id:number,lectureDataList:Lecture[]):Lecture {
+  var lectureData:Lecture=lectureDataList[0];
+  for(var i = 0; i < lectureDataList.length ; i++){
+    if(lectureDataList[i].id == id){
+      lectureData=lectureDataList[i];
+    }
+  }
+  return lectureData;
+}
 
 const sampleLectureDates: LectureDate[] = [
   { dayOfWeek: 1, period: [1, 2] },
