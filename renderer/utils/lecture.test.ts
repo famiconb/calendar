f1f4a6d6a07ad165479d77db19f3afcf73d954f4
Nagarei <Nagarei@users.noreply.github.com>
2022-06-14 15:35:15 +0900

    quater引数を追加

--- renderer/utils/lecture.test.ts
+++ renderer/utils/lecture.test.ts
@@ -1,8 +1,8 @@
 import { Lecture } from "../interfaces";
 import { saveLecture, loadLecture } from "./lecture";
 
-function saveLectureTest() {
-  const testdata: Lecture[] = [
+function saveLectureTest(quater: number = 0) {
+  const testdata0: Lecture[] = [
     {
       id: 0,
       name: "こうぎ1",
@@ -16,13 +16,40 @@ function saveLectureTest() {
       memo: [{ title: "zoom", text: "https://B" }],
     },
   ];
-
+  const testdata1: Lecture[] = [
+    {
+      id: 4,
+      name: "こうぎ4",
+      dates: [
+        { dayOfWeek: 1, period: [3, 4] },
+        { dayOfWeek: 3, period: [1, 2] },
+      ],
+      memo: [{ title: "zoom", text: "https://C" }],
+    },
+    {
+      id: 5,
+      name: "こうぎ5",
+      dates: [{ dayOfWeek: 4, period: [1, 2, 5, 6] }],
+      memo: [{ title: "zoom", text: "https://D" }],
+    },
+  ];
   localStorage.clear();
-  saveLecture(testdata);
-  loadLectureTest(testdata);
+  saveLecture(testdata0, quater);
+  loadLectureTest(testdata0, quater);
+  loadLectureTest([], (quater + 1) % 4);
+  loadLectureTest([], (quater + 2) % 4);
+  loadLectureTest([], (quater + 3) % 4);
+
+  saveLecture(testdata1, (quater + 1) % 4);
+  loadLectureTest(testdata0, quater);
+  loadLectureTest(testdata1, (quater + 1) % 4);
+  loadLectureTest([], (quater + 2) % 4);
+  loadLectureTest([], (quater + 3) % 4);
+
+  expect(Object.keys(localStorage.__STORE__).length).toBe(2);
 }
-function loadLectureTest(expect_lec: Lecture[]) {
-  const data: Lecture[] = loadLecture();
+function loadLectureTest(expect_lec: Lecture[], quater: number) {
+  const data: Lecture[] = loadLecture(quater);
   const datastr: string = JSON.stringify(data);
   const expectstr: string = JSON.stringify(expect_lec);
   expect(datastr).toBe(expectstr);
@@ -33,8 +60,7 @@ describe("test lecture saveload", () => {
     // このライブラリ使うのやめて新しくモック作るのも一案かもしれない
     // @ts-ignore
     localStorage.setItem.mockClear();
-    loadLectureTest([]);
+    loadLectureTest([], 0);
     saveLectureTest();
-    expect(Object.keys(localStorage.__STORE__).length).toBe(1);
   });
 });
--- renderer/utils/lecture.ts
+++ renderer/utils/lecture.ts
@@ -1,16 +1,19 @@
 import { Lecture } from "../interfaces";
 
-const STORAGE_KEY = "CUCalendar";
+const STORAGE_KEY = "CUCalendar_";
 
-export function loadLecture(): Lecture[] {
-  const data_str = localStorage.getItem(STORAGE_KEY);
+/**
+ * @param quater: 0,1,2,3のどれか
+ */
+export function loadLecture(quater: number = 0): Lecture[] {
+  const data_str = localStorage.getItem(STORAGE_KEY + quater.toString());
   if (data_str === null) {
     return [];
   }
   return JSON.parse(data_str) as Lecture[];
 }
-export function saveLecture(lectures: Lecture[]) {
+export function saveLecture(lectures: Lecture[], quater: number = 1) {
   const data_str: string = JSON.stringify(lectures);
-  localStorage.setItem(STORAGE_KEY, data_str);
+  localStorage.setItem(STORAGE_KEY + quater.toString(), data_str);
   return;
 }
