4f2e041bd93c542b80f9b66329616d173b57cfc5
Nagarei <Nagarei@users.noreply.github.com>
2022-06-14 17:09:21 +0900

    headerをquarter対応

--- renderer/components/Layout.tsx
+++ renderer/components/Layout.tsx
@@ -2,41 +2,43 @@ import React, { ReactNode } from "react";
 import Link from "next/link";
 import Head from "next/head";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useQuarter } from "../hooks/useQuarter";
 
 type Props = {
   children: ReactNode;
   title?: string;
 };
 
-const Layout = ({ children, title = "This is the default title" }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{" "}
-        |{" "}
-        <Link href="/add-page">
-          <a>講義追加</a>
-        </Link>{" "}
-        |{" "}
-        <Link href="/lecture-info?id=1">
-          <a>講義情報(DEBUG)</a>
-        </Link>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      {/* ここがフッター */}
-      <span></span>
-    </footer>
-  </div>
-);
+const Layout = ({ children, title = "This is the default title" }: Props) => {
+  // queryパラメータからquarterを取る
+  const quarter: number = useQuarter();
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header>
+        <nav>
+          <Link href={`/?quarter=${quarter}`}>
+            <a>Home</a>
+          </Link>{" "}
+          |{" "}
+          <Link href={`/add-page?quarter=${quarter}`}>
+            <a>講義追加</a>
+          </Link>
+        </nav>
+      </header>
+      {children}
+      <footer>
+        <hr />
+        {/* ここがフッター */}
+        <span></span>
+      </footer>
+    </div>
+  );
+};
 
 export default Layout;
