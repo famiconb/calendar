f0c9a5f11d36ab2ef5eee899831cb37ed2e2e1aa
Shunsuke Uchino <news7vip@gmail.com>
2022-06-14 15:56:58 +0900

    Simplify index.tsx

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -2,6 +2,47 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 import { Lecture } from "../interfaces";
 import { useLectureData } from "../hooks/useLectureData";
+import React from "react";
+
+/// 各表の行を表示する
+/// number 何限目
+/// lecture 講義データ Demo
+const row_view = (number: number, lectures: Lecture[]) => {
+  // 時間割のnumber限目の列
+  const row = [];
+
+  // dayOfWeek 何曜日
+  for (let dayOfWeek = 0; dayOfWeek < 6; dayOfWeek++) {
+    if (dayOfWeek == 0) {
+      row.push(<th style={{ border: "solid 1px" }}>{number + "時限目"}</th>);
+    } else {
+      let found = false;
+      for (let lectures_i = 0; lectures_i < lectures.length; ++lectures_i) {
+        const lecture: Lecture = lectures[lectures_i];
+        for (let i = 0; i < lecture.dates.length; ++i) {
+          for (let j = 0; j < lecture.dates[i].period.length; ++j) {
+            if (
+              lecture.dates[i].period[j] == number &&
+              lecture.dates[i].dayOfWeek == dayOfWeek
+            ) {
+              row.push(
+                <th style={{ height: "50px", border: "solid 1px" }}>
+                  <Link href={`/lecture-info?id=${lecture.id}`}>
+                    {lecture.name}
+                  </Link>
+                </th>
+              );
+              found = true;
+            }
+          }
+        }
+      }
+      if (!found)
+        row.push(<th style={{ height: "50px", border: "solid 1px" }}></th>);
+    }
+  }
+  return <tr>{row}</tr>;
+};
 
 //// 表示用の講義データ
 // const lectures: Lecture[] = [
@@ -13,6 +54,10 @@ import { useLectureData } from "../hooks/useLectureData";
 //   },
 // ];
 
+const TableHead: React.FC<{ children?: React.ReactNode }> = (props) => (
+  <th className="border border-solid border-black w-28">{props.children}</th>
+);
+
 const IndexPage = () => {
   // イベントクリック 今は無用のもの
   // const onSelectEvent = useCallback((calEvent) => {
@@ -22,66 +67,32 @@ const IndexPage = () => {
   // 表示用の講義データ
   const { lectures } = useLectureData();
 
-  // 各表の行を表示する
-  // number 何限目
-  // lecture 講義データ Demo
-  const row_view = (number: number, lectures: Lecture[]) => {
-    // 時間割のnumber限目の列
-    const row = [];
-
-    // dayOfWeek 何曜日
-    for (let dayOfWeek = 0; dayOfWeek < 6; dayOfWeek++) {
-      if (dayOfWeek == 0) {
-        row.push(<th style={{ border: "solid 1px" }}>{number + "時限目"}</th>);
-      } else {
-        let found = false;
-        for (let lectures_i = 0; lectures_i < lectures.length; ++lectures_i) {
-          const lecture: Lecture = lectures[lectures_i];
-          for (let i = 0; i < lecture.dates.length; ++i) {
-            for (let j = 0; j < lecture.dates[i].period.length; ++j) {
-              if (
-                lecture.dates[i].period[j] == number &&
-                lecture.dates[i].dayOfWeek == dayOfWeek
-              ) {
-                row.push(
-                  <th style={{ height: "50px", border: "solid 1px" }}>
-                    <Link href={`/lecture-info?id=${lecture.id}`}>
-                      {lecture.name}
-                    </Link>
-                  </th>
-                );
-                found = true;
-              }
-            }
-          }
-        }
-        if (!found)
-          row.push(<th style={{ height: "50px", border: "solid 1px" }}></th>);
-      }
-    }
-    return <tr>{row}</tr>;
-  };
-
   return lectures != null ? (
     <Layout title="CUCalendar">
-      <table style={{ border: "solid 1px" }}>
-        <tr>
-          <th style={{ width: "100px", border: "solid 1px" }}></th>
-          <th style={{ width: "100px", border: "solid 1px" }}>月</th>
-          <th style={{ width: "100px", border: "solid 1px" }}>火</th>
-          <th style={{ width: "100px", border: "solid 1px" }}>水</th>
-          <th style={{ width: "100px", border: "solid 1px" }}>木</th>
-          <th style={{ width: "100px", border: "solid 1px" }}>金</th>
-        </tr>
-        {row_view(1, lectures)}
-        {row_view(2, lectures)}
-        {row_view(3, lectures)}
-        {row_view(4, lectures)}
-        {row_view(5, lectures)}
-        {row_view(6, lectures)}
-        {row_view(7, lectures)}
-        {row_view(8, lectures)}
-      </table>
+      <div className="p-2 w-screen h-screen">
+        <table className="border border-solid w-full h-full">
+          <thead>
+            <tr>
+              <TableHead></TableHead>
+              <TableHead>月</TableHead>
+              <TableHead>火</TableHead>
+              <TableHead>水</TableHead>
+              <TableHead>木</TableHead>
+              <TableHead>金</TableHead>
+            </tr>
+          </thead>
+          <tbody>
+            {row_view(1, lectures)}
+            {row_view(2, lectures)}
+            {row_view(3, lectures)}
+            {row_view(4, lectures)}
+            {row_view(5, lectures)}
+            {row_view(6, lectures)}
+            {row_view(7, lectures)}
+            {row_view(8, lectures)}
+          </tbody>
+        </table>
+      </div>
     </Layout>
   ) : (
     <div>loading...</div>
