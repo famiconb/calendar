b640ea94c4c67d9c787a806385f52a4220a48b25
Nagarei <Nagarei@users.noreply.github.com>
2022-06-28 14:55:44 +0900

    remove default value

--- renderer/utils/lecture.ts
+++ renderer/utils/lecture.ts
@@ -5,14 +5,14 @@ const STORAGE_KEY = "CUCalendar_";
 /**
  * @param quarter: 0,1,2,3のどれか
  */
-export function loadLecture(quarter: number = 0): Lecture[] {
+export function loadLecture(quarter: number): Lecture[] {
   const data_str = localStorage.getItem(STORAGE_KEY + quarter.toString());
   if (data_str === null) {
     return [];
   }
   return JSON.parse(data_str) as Lecture[];
 }
-export function saveLecture(lectures: Lecture[], quarter: number = 0) {
+export function saveLecture(lectures: Lecture[], quarter: number) {
   const data_str: string = JSON.stringify(lectures);
   localStorage.setItem(STORAGE_KEY + quarter.toString(), data_str);
   return;
