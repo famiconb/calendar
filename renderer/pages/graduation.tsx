a696c17fb6373d2513c98c833c9de53e56d46962
Nagarei <Nagarei@users.noreply.github.com>
2022-07-05 17:54:07 +0900

    入力データから科目コードを抽出

--- renderer/pages/graduation.tsx
+++ renderer/pages/graduation.tsx
@@ -8,15 +8,33 @@ import Button from "../components/Button";
 import { useQuarter } from "../hooks/useQuarter";
 
 
+// 引数のlecture群から科目コードを抽出
+const get_class_codes = (lectures_allq: Lecture[][]):string => {
+  const codes: string[] = []
+  for (let index_lectures_allq = 0; index_lectures_allq < lectures_allq.length; index_lectures_allq++) {
+    const lectures = lectures_allq[index_lectures_allq];
+    for (let index = 0; index < lectures.length; index++) {
+      const lecture: Lecture = lectures[index];
+      if (lecture.code != null && lecture.code !== "") {
+        codes.push(lecture.code);
+      }
+    }
+  }
+  console.log(codes)
+  return codes.join(",")
+}
+
 const GraduationPage = () => {
 
   const router = useRouter();
   const quarter: number = useQuarter();
-  const lectures_allq: (Lecture[] | undefined)[] = []
-  lectures_allq[0] = useLectureData(0).lectures;
-  lectures_allq[1] = useLectureData(1).lectures;
-  lectures_allq[2] = useLectureData(2).lectures;
-  lectures_allq[3] = useLectureData(3).lectures;
+  const lectures0 = useLectureData(0).lectures;
+  const lectures1 = useLectureData(1).lectures;
+  const lectures2 = useLectureData(2).lectures;
+  const lectures3 = useLectureData(3).lectures;
+  // const is_initialized_all = ():boolean => {
+  //   return (lectures0 != null && lectures1 != null && lectures2 != null && lectures3 != null);
+  // }
 
   //pythonとの通信
   useEffect(() => {
@@ -35,25 +53,19 @@ const GraduationPage = () => {
   const onClick = () => {
     const course = '情報工学コース修士課程';
     const determine_course = '情報工学コース修士課程';
-    const class_codes = 'LAH.S433,LAH.T420,LAH.S501,LAC.M401,LAC.M527,CSC.Z491,CSC.Z492,CSC.Z591,CSC.Z592,CSC.U481,CSC.U482,CSC.T421,CSC.T422,CSC.T426,ART.T458,XCO.T484,XCO.T473,XCO.T474,XCO.T478';
-
-    global.ipcRenderer.send('check_graduation', [course, determine_course, class_codes])
-  }
-  const onClickFalse = () => {
-    const course = '情報工学コース修士課程';
-    const determine_course = '情報工学コース修士課程';
-    const class_codes = 'CSC.Z491,CSC.Z492,CSC.Z591,CSC.Z592,CSC.U481,CSC.U482,CSC.T421,CSC.T422,CSC.T426,ART.T458,XCO.T484,XCO.T473,XCO.T474,XCO.T478';
-
-    global.ipcRenderer.send('check_graduation', [course, determine_course, class_codes])
+    //const class_codes = 'LAH.S433,LAH.T420,LAH.S501,LAC.M401,LAC.M527,CSC.Z491,CSC.Z492,CSC.Z591,CSC.Z592,CSC.U481,CSC.U482,CSC.T421,CSC.T422,CSC.T426,ART.T458,XCO.T484,XCO.T473,XCO.T474,XCO.T478';
+    if (lectures0 != null && lectures1 != null && lectures2 != null && lectures3 != null) {
+      const class_codes = get_class_codes( [lectures0, lectures1, lectures2, lectures3]);
+      global.ipcRenderer.send('check_graduation', [course, determine_course, class_codes]);
+    }
   }
 
-  return (lectures_allq[0] != null && lectures_allq[1] != null && lectures_allq[2] != null && lectures_allq[3] != null) ? (
+  return (lectures0 != null && lectures1 != null && lectures2 != null && lectures3 != null) ? (
     <Layout
       title="修了判定"
       goBack={() => router.push("/?quarter=" + quarter.toString())}
     >
       <button onClick={onClick}>[YES]</button>
-      <button onClick={onClickFalse}>[NO]</button>
     </Layout>
   ) : (
     <div>loading...</div>
